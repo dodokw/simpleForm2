f7ea84bba6a26dd4d89705765c657cee3d648559
dodokw <dodokw@naver.com>
2022-12-01 15:43:59 +0900

    first_commiut

---
+++ App.tsx
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView style={{}}>
+      <StatusBar barStyle={'light-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        style={{}}>
+        <Header />
+        <View
+          style={{
+            backgroundColor: Colors.white
+          }}>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
